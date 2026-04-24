3cad93c6eb42519ebb16e92d19f2e5833fdead16
prabeshbuilds <prabeshuser407@gmail.com>
2026-04-24 21:43:57 +0545

    some added

--- config/settings.py
+++ config/settings.py
@@ -80,14 +80,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'health',
-        'USER': 'health_user',
-        'PASSWORD': 'health123',
-        'HOST': 'localhost',
+        'NAME': os.getenv('POSTGRES_DB', 'health'),
+        'USER': os.getenv('POSTGRES_USER', 'health_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'health123'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
         'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
@@ -123,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
